964368d0c5175c5b973d01bcb94dee8588e38e5b
Marcaronii <marcdavidgonzales8@gmail.com>
2026-08-18 17:42:06 +0800

    portal

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // (Or Vue, Svelte, etc.)
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+})
